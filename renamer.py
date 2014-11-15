bf2874e82131f0a67707a3a4afb9fe58cd2ed836
Yoann Gesquiere <yoann.gesquiere@gmail.com>
2014-11-15 16:39:05 +0100

    Simplification using getopt

--- renamer.py
+++ renamer.py
@@ -1,36 +1,35 @@
 import os
 import glob
 import sys
+import getopt
 
 #Function to rename file
 def _renameFile(originalFile, newName):
     os.rename(originalFile, newName)
 
+#Function that renames all the images in a folder
 def _renameMultipleFiles(folder, extension, prefix):
-    sFilePrefix = prefix
     iIncrement = 1
     l = glob.glob(folder+"/*."+extension)
     
     for element in l:
         if os.path.isfile(element) and (os.path.basename(element)).endswith(extension) > 0 :
-            _renameFile(element, folder+"/"+sFilePrefix+str(iIncrement).zfill(5)+"."+extension)
+            _renameFile(element, folder+"/"+prefix+str(iIncrement).zfill(5)+"."+extension)
             iIncrement = iIncrement + 1
 
-OPTION_PREFIX = "--"
-OPTION_EQUAL = "="
-aListOptions = {}
-FOLDER_OPTION = "folder"
-PREFIX_OPTION = "prefix"
-for argument in sys.argv:
-    if argument.find(OPTION_PREFIX) == 0 and argument.find(OPTION_EQUAL) > 0 :
-        option = argument[2:argument.index(OPTION_EQUAL)]
-        value = argument[argument.index(OPTION_EQUAL)+1:]
-        aListOptions[option] = value
 folder = './'
 prefix = 'new_'
-if FOLDER_OPTION in aListOptions.keys():
-    folder = aListOptions[FOLDER_OPTION]
-if PREFIX_OPTION in aListOptions.keys():
-    prefix = aListOptions[PREFIX_OPTION]
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "", ["prefix=", "folder="])
+except getopt.GetoptError:
+    print('KO')
+    exit(2)
+
+for opt, arg in opts:
+    if opt in ("--prefix"):
+        prefix = arg
+    elif opt in ("--folder"):
+        folder = arg
 
 _renameMultipleFiles(folder, 'JPG', prefix)
